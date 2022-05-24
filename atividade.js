9f68ad03360f222a7eef9eb2901f5e848eb2434e
Gabrielsilvay <dasilvagabriel22@gmail.com>
2022-05-24 19:28:28 -0300

    Atividade online 24/05

---
+++ atividade.js
@@ -0,0 +1,23 @@
+///Sistema de cadastro
+
+var hoje = new Date();
+const data = ['Data permitida','data não permitida',]
+console.log('Data:'+hoje)
+
+if (data.length < hoje) {
+    console.log('Data permitida');
+}
+for (let index = 0; index < hoje.length; index++) {
+    const dataatual = hoje[index];
+    if (pecaatual.length > 24/05/2022) {
+        console.log(dataatual + ':Data invalida')
+    }else
+console.log(dataatual + ': Data permitida')
+    }
+
+    var idade= 19;
+    console.log('Idade: '+ idade)
+    if (idade >= 18) {
+        console.log('Idade permitida')
+    }else
+    console.log('Idade não permitida')
